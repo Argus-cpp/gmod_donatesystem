608ea8d8f48bdad8e189504b6a374c8137b00d40
Oleg <favarplay@gmail.com>
2021-04-20 23:14:11 +0300

    Update sv_core.lua

--- lua/dustcode_core/sv_core.lua
+++ lua/dustcode_core/sv_core.lua
@@ -244,7 +244,7 @@ local function CheckPayments()
 end
 
 local function CheckVersion()
-	http.Fetch("http://gmoddustcode.ru/donatesystem/scripts/version.php", function(data)
+	http.Fetch("https://raw.githubusercontent.com/FriksGit/gmod_donatesystem/".._DUSTCODE_DONATE.VersionType.."/version", function(data)
 		if data != _DUSTCODE_DONATE.Version then
 			_DUSTCODE_DONATE:Log("Новая версия "..data.." доступна для скачивания, текущая ".._DUSTCODE_DONATE.Version, true)
 		end
